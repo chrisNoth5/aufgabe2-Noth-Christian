5f4b53ea3a9f5ca6c78946fc499dcf9db5622893
Joshua Dreessen <je.dreessen@gmail.com>
2021-10-27 21:23:43 +0200

    Finish test cases

--- src/de/uni/hd/pokemon/PokemonTest.java
+++ src/de/uni/hd/pokemon/PokemonTest.java
@@ -46,5 +46,76 @@ public class PokemonTest {
         System.out.println(p1.toString());
         System.out.println("Rufe p1.toString() implizit auf:");
         System.out.println(p1);
+
+        Pokemon p2 = new Pokemon("Pamflam", Type.POISON);
+        Pokemon p3 = new Pokemon("Glurak", Type.FIRE);
+        Pokemon p4 = new Pokemon("Magikarp", Type.WATER);
+
+        // Test of class constructor
+        System.out.println();
+        System.out.println("- Testen von constructor -");
+        Trainer trainer = new Trainer("Ash", "Ketchum");
+
+        // Test of linkPokemon method
+        System.out.println("- Testen von linkPokemon -");
+        System.out.println("Füge Pokemon p2 der Liste von Trainer trainer hinzu:");
+        trainer.linkPokemon(p2);
+        System.out.println("Pokemon p2 wurde der Liste von Trainer trainer hinzugefügt.");
+        System.out.println("Prüfe auf Erfolg von linkPokemon:");
+        System.out.println(trainer.listPokemon());
+
+        trainer.linkPokemon(p3);
+        trainer.linkPokemon(p4);
+
+        // Test of getter of firstName
+        System.out.println();
+        System.out.println("- Testen von getFirstName -");
+        System.out.println("Vorname von Trainer trainer:");
+        System.out.println(trainer.getFirstName());
+
+        // Test of setter of firstName
+        System.out.println();
+        System.out.println("- Testen von setFirstName -");
+        System.out.println("Setze Vorname von Trainer trainer von 'Ash' auf 'Max'.");
+        trainer.setFirstName("Max");
+        System.out.println("Vorname von Trainer trainer wurde zu 'Max' umbenannt.");
+        System.out.println("Prüfe auf Erfolg von setFirstName:");
+        System.out.println(trainer.getFirstName());
+
+        // Test of getter of lastName
+        System.out.println();
+        System.out.println("- Testen von getLastName -");
+        System.out.println("Nachname von Trainer trainer:");
+        System.out.println(trainer.getLastName());
+
+        // Test of setter of lastName
+        System.out.println();
+        System.out.println("- Testen von setLastName -");
+        System.out.println("Setze Nachname von Trainer trainer von 'Ketchum' auf 'Mustermann'.");
+        trainer.setLastName("Max");
+        System.out.println("Nachname von Trainer trainer wurde zu 'Mustermann' umbenannt.");
+        System.out.println("Prüfe auf Erfolg von setLastName:");
+        System.out.println(trainer.getLastName());
+
+        // Test of toString method
+        System.out.println();
+        System.out.println("- Testen der toString-Methode -");
+        System.out.println("Rufe trainer.toString() implizit auf:");
+        System.out.println(trainer);
+
+        // Test of listPokemon method
+        System.out.println("- Testen der listPokemon-Methode -");
+        System.out.println("Gebe Liste der Pokemon von trainer aus:");
+        System.out.println(trainer.listPokemon());
+
+        // Test of listPokemonByType method
+        System.out.println("- Testen der listPokemonByType-Methode -");
+        System.out.println("Gebe Liste der Pokemon von trainer vom Typ 'WATER' aus:");
+        System.out.println(trainer.listPokemonByType(Type.WATER));
+
+        // Test of getPokemonByIndex method
+        System.out.println("- Testen der getPokemonByIndex-Methode -");
+        System.out.println("Gebe zweites Pokemon von trainer aus:");
+        System.out.println(trainer.getPokemonByIndex(1));
     }
 }
